49e76b80b5d7457c86772e9837ee7aeb4bea5f84
Enrique Pelaez Garcia <enriquepelaez12@outlook.com>
2019-03-21 14:16:18 -0600

    se crearon los componentes y las carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { ProfileComponent } from './components/users/profile/profile.component';
+import { RegisterComponent } from './components/users/register/register.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductComponent } from './components/products/product/product.component';
+import { ProductListComponent } from './components/products/product-list/product-list.component';
+import { PrivadoComponent } from './components/privado/privado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    Page404Component,
+    ProductsComponent,
+    ProductComponent,
+    ProductListComponent,
+    PrivadoComponent
   ],
   imports: [
     BrowserModule,
