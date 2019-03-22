8c86151620aef7c9b11b688dd05742a0686ae6e0
Enrique Pelaez Garcia <enriquepelaez12@outlook.com>
2019-03-21 20:36:26 -0600

    se agrego la funcion de login y tiene notificaciones

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import { from } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public afAuth: AngularFireAuth,
+    private router: Router,
+    private authService: AuthService,
+    private flashMessage: FlashMessagesService //para los mensajes de confirmacion
+    ) { }
 
+    public email: string = '';
+    public password: string = '';
   ngOnInit() {
+  }
+
+  onLogin(): void {
+  this.authService.loginEmailUser(this.email, this.password)
+    .then((res)=>{
+      this.flashMessage.show('A ingresado Correctamente', //para mostrar mensaje
+      {cssClass: 'alert-success', timeout: 4000});//para mostrar mensaje deque entro correctamente
+    this.onLoginRedirect();
+  }).catch(err => 
+    this.flashMessage.show(err.message, //para mostrar mensaje
+      {cssClass: 'alert-danger', timeout: 4000}) //para mostrar mensaje
+    )};
+  //catch(err => console.log('err', err.message));
+
+
+
+
+  //metodo para salir 
+  onLogout() {
+    this.authService.logoutUser();
+  }
+
+  //
+  onLoginRedirect(){
+    this.router.navigate(['privado']);//si esta logueado se manda a esa direccion
+
   }
 
 }
