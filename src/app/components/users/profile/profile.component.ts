13b1e090148e2c764722af310317f2d6b03f935f
Enrique Pelaez Garcia <enriquepelaez12@outlook.com>
2019-03-27 00:35:00 -0600

    se agrego el perfil a la app Web

--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,3 +1,5 @@
+import { UserInterface } from './../../../models/user';
+import { AuthService } from './../../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthService) { }
+    //inicializamos  los atributos de la interfaz user
+    user: UserInterface = {
+      name: '',
+      email: '',
+      photoUrl: '',
+     // roles: {}
+    };
+    
+    public providerId: string = 'null'; 
+     ngOnInit() {
+       //se comprueba si el usuario esta logado
+       this.authService.isAuth().subscribe(user =>{
+         if (user){
+           this.user.name = user.displayName;
+           this.user.email = user.email;
+           this.user.photoUrl = user.photoURL;
+           this.providerId = user.providerData[0].providerId;
+         }
+       })
   }
 
 }
